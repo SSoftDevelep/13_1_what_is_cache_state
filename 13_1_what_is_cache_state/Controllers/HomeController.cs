8fcf334b54f7fb26530979658a249f7424b110b6
SsoftDevelep <saitemresolmaz@gmail.com>
2017-03-01 15:44:33 +0300

    Olum tarihli cache

---
+++ 13_1_what_is_cache_state/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _13_1_what_is_cache_state.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            //HttpContext.Cache["ad"] = "Brad Pitt";
+            //HttpContext.Cache.Remove("ad"); //silme islemi
+
+            //HttpContext.Cache.Add("ad", "Brad Pitt", null, System.Web.Caching.Cache.NoAbsoluteExpiration, new TimeSpan(0, 0, 10),System.Web.Caching.CacheItemPriority.Normal,null);  //ölüm tarihi yok(NoAbsoulteExpiration) ve son erisimden 10 sn sonra temizlencek.
+            HttpContext.Cache.Add("ad", "Brad Pitt", null,new DateTime(2017,3,1,15,43,0), System.Web.Caching.Cache.NoSlidingExpiration, System.Web.Caching.CacheItemPriority.Normal, null);
+
+            return View();
+        }
+
+
+        public ActionResult Index2()
+        {
+            return View();
+        }
+    }
+}
