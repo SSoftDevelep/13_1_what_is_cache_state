1fcc2fe8413699d6684802f8af2c6e8b679b0f0e
SsoftDevelep <saitemresolmaz@gmail.com>
2017-03-01 16:23:18 +0300

    Cache Cache Callback Fonksiyonu

--- 13_1_what_is_cache_state/Controllers/HomeController.cs
+++ 13_1_what_is_cache_state/Controllers/HomeController.cs
@@ -14,15 +14,30 @@ namespace _13_1_what_is_cache_state.Controllers
             //HttpContext.Cache["ad"] = "Brad Pitt";
             //HttpContext.Cache.Remove("ad"); //silme islemi
 
-            //HttpContext.Cache.Add("ad", "Brad Pitt", null, System.Web.Caching.Cache.NoAbsoluteExpiration, new TimeSpan(0, 0, 10),System.Web.Caching.CacheItemPriority.Normal,null);  //ölüm tarihi yok(NoAbsoulteExpiration) ve son erisimden 10 sn sonra temizlencek.
-            HttpContext.Cache.Add("ad", "Brad Pitt", null,new DateTime(2017,3,1,15,43,0), System.Web.Caching.Cache.NoSlidingExpiration, System.Web.Caching.CacheItemPriority.Normal, null);
+            HttpContext.Cache.Add("ad", "Brad Pitt", null, System.Web.Caching.Cache.NoAbsoluteExpiration, new TimeSpan(0, 0, 10),System.Web.Caching.CacheItemPriority.Normal,CacheItemExpired);  //ölüm tarihi yok(NoAbsoulteExpiration) ve son erisimden 10 sn sonra temizlencek.
+            //HttpContext.Cache.Add("ad", "Brad Pitt", null,new DateTime(2017,3,1,15,43,0), System.Web.Caching.Cache.NoSlidingExpiration, System.Web.Caching.CacheItemPriority.Normal,CacheItemExpired); //belirtilen tarihte ölcek.
 
+           
             return View();
+        }
+
+
+        private void CacheItemExpired(string key,object value,System.Web.Caching.CacheItemRemovedReason reason)
+        {
+            //Mail de atilabilir ..
+            System.IO.File.WriteAllText(Server.MapPath("~/cache-expired-note.txt"), $"{key} cache item and {value.ToString()} cache item value are expired. Reason : {reason.ToString()}");
         }
 
 
         public ActionResult Index2()
         {
+            return View();
+        }
+
+
+        public ActionResult Index3()
+        {
+            HttpContext.Cache.Remove("ad");
             return View();
         }
     }
